3a23c0e828a4e6871042b6d6b8d17ec97c617c70
Igor Pinheiro <igor11pinheiro@gmail.com>
2023-09-17 16:29:12 -0300

    Classe Endereco

--- codigo/backend/Cliente.API/Models/Endereco.cs
+++ codigo/backend/Cliente.API/Models/Endereco.cs
@@ -1,6 +1,32 @@
-﻿namespace Cliente.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cliente.API.Models;
 
 public class Endereco
 {
+    [Required]
+    [MinLength(3), MaxLength(200)]
+    public string Rua { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(1), MaxLength(6)]
+    public int Numero { get; set; }
+
+    [Required]
+    [MinLength(2), MaxLength(50)]
+    public string Bairro { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(2), MaxLength(50)]
+    public string Cidade { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(2), MaxLength(50)]
+    public string Estado { get; set; } = string.Empty;
+
+    [Key]
+    [Required]
+    [MinLength(8), MaxLength(8)]
+    public string CEP { get; set; } = string.Empty;
 
 }
