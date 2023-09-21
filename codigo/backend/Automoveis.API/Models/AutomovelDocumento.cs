2c84344f703c8d52d54243f1c2ded4d7dc1af170
Igor Pinheiro <igor11pinheiro@gmail.com>
2023-09-20 21:00:36 -0300

    Automovel API

---
+++ codigo/backend/Automoveis.API/Models/AutomovelDocumento.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Automoveis.API.Models
+{
+    public class AutomovelDocumento
+    {
+        [JsonProperty("renavam")]
+        public string Renavam { get; set; } = string.Empty;
+
+        [JsonProperty("ano")]
+        public int Ano { get; set; }
+
+        [JsonProperty("marca")]
+        public string marca { get; set; } = string.Empty;
+
+        [JsonProperty("modelo")]
+        public string modelo { get; set; } = string.Empty;
+
+        [JsonProperty("placa")]
+        public string placa { get; set;} = string.Empty;
+
+        [JsonProperty("mensalidade")]
+        public decimal mensalidade { get; set; }
+
+    }
+}
